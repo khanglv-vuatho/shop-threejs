c049dc9531ba93cf6d3af531870b0a9084c9534b
khanglv-vuatho <khanglv@vuatho.com>
2024-01-09 23:01:56 +0700

    doneee

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
-const withNextIntl = require('next-intl/plugin')();
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-module.exports = withNextIntl({
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'flagcdn.com' },
-      { protocol: 'https', hostname: 'vuatho.com' },
-      { protocol: 'https', hostname: 'cdn.vuatho.com' },
-      { protocol: 'https', hostname: 'icdn.vuatho.com' },
-      { protocol: 'https', hostname: 'api.vuatho.com' },
-      { protocol: 'http', hostname: 'localhost' },
-      { protocol: 'https', hostname: 'sandbox-api-cms.vuatho.com' },
-      { protocol: 'http', hostname: '192.168.1.72' },
-    ],
-  },
-});
+module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
+import { motion, AnimatePresence } from 'framer-motion'
+import { useSnapshot } from 'valtio'
+import state from '@/storeValtio'
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from '@/motion'
+import Image from 'next/image'
+
 export default function Home() {
-  return <div>123</div>
+  const snap = useSnapshot(state)
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.div className='home' {...slideAnimation('left')}>
+          <motion.header>
+            <Image
+              src={'./threejs.png'}
+              alt='logo'
+              width={50}
+              height={32}
+              className='size-8 object-contain'
+            />
+          </motion.header>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  )
 }
